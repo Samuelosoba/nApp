e071073bcd61ac04bbf0339f0c40c89007620e8a
osobasamuel <samuelosoba7@gmail.com>
2026-05-18 23:45:00 +0100

    .

--- app/about.tsx
+++ app/about.tsx
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Link } from 'expo-router'
 
 const About = () => {
   return (
     <View>
       <Text>Aabout</Text>
+      <Link href="/index" >Home</Link>
     </View>
   )
 }
